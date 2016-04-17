62575581145927de4f270ed138efb70b16ff679c
Hans Buddenberg <hans.buddenberg@me.com>
2016-04-17 15:10:40 -0300

    Archivos Base modificados
    La base de archvios corresspondiente a Wordpress estan funcionales a nivel basico.

---
+++ footer.php
@@ -0,0 +1,17 @@
+			<footer>
+				<p><!-- &amp;amp;amp;copy; -->
+					&nbsp;&nbsp;
+					<!-- &copy;<?php bloginfo('name'); ?>, <?=date('m-Y');?>. Hans-Dieter Buddenberg Blamey. -->
+					<?php bloginfo('name'); ?>, <?=date('Y');?> &copy; Hans-Dieter Buddenberg Blamey.
+					&nbsp;&nbsp;
+				</p>
+
+			</footer>
+
+		</div>
+		<!-- Fin de wrapper -->
+
+		<?php wp_footer(); ?>
+	</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<!-- Inicio de main -->
+<section id="main">
+
+</section>
+<!-- Fin de main -->
+
+<?php  get_sidebar()?>
+
+<?php get_footer(); ?>
